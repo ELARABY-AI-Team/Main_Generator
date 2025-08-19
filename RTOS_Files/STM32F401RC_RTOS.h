e25f82c39d4a9bb8cdf99a2f484dee890315f5fc
omar-magdy1 <o-magdy@elarabygroup.com>
2025-08-19 15:54:50 +0300

    Add RTOS file STM32F401RC_RTOS.h for STM32F401RC

---
+++ RTOS_Files/STM32F401RC_RTOS.h
@@ -0,0 +1,14 @@
+/******************************************************************
+* @Title    : TIME TRIGGERED Scheduler (TTSCH)                        
+* @Filename : schedule.h                              
+* @Author   : Ahmed Magdy                     
+* @Origin Date : 12/10/2023                                                                    
+* @Version  : 1.0.0                                                                           
+* @Compiler : GCC                                                                           
+* @Target   : R5F11BBC                                                                     
+* @brief    : This file contains the prototypes of the SCH Module                         
+* @Notes    : You have to configure here the number of tasks                                        
+*******************************************************************/
+					         
+					         
+
