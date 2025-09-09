6d7902a0fe4755b5078b9f3ab7b8ed3171bf91e3
omar-magdy1 <o-magdy@elarabygroup.com>
2025-09-09 09:53:49 +0300

    Add files via upload

---
+++ RTOS_Files/Source/FreeRTOS_user.h
@@ -0,0 +1,15 @@
+#define configUSE_PREEMPTION                     1
+#define configCPU_CLOCK_HZ                       ( SystemCoreClock )
+#define configTICK_RATE_HZ                       ((TickType_t)1000)
+#define configMAX_PRIORITIES                     ( 56 )
+#define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
+#define configMAX_TASK_NAME_LEN                  ( 16 )
+#define configUSE_16_BIT_TICKS                   0
+#define configUSE_MUTEXES                        1
+#define configQUEUE_REGISTRY_SIZE                8
+#define configUSE_RECURSIVE_MUTEXES              1
+#define configUSE_COUNTING_SEMAPHORES            1
+
+
+#define configTIMER_QUEUE_LENGTH                 10
+#define configTIMER_TASK_STACK_DEPTH             256
